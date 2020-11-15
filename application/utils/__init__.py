3f662349ae54d0bb6c669094f0afa3224a39414b
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-15 11:19:06 +0700

    Adding util function to get date now

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -1,7 +1,12 @@
 import os
 
+from datetime import datetime
 
 # This function to get .env variable configuration
 # @params var - fill with the same variable name in .env configuration
 def get_env(var: str):
     return os.environ.get(var)
+
+# This function to get date time now with custom format
+def date_now(pattern: str):
+    return datetime.now().strftime(pattern)
