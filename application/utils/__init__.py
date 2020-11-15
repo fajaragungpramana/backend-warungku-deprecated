dcab39d54e0e23e1bc2cbad60fbda3b336f20612
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-15 10:42:40 +0700

    Adding util function to get .env variable configuration

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -1,1 +1,7 @@
+import os
 
+
+# This function to get .env variable configuration
+# @params var - fill with the same variable name in .env configuration
+def get_env(var: str):
+    return os.environ.get(var)
