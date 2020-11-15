c7ee99572ed05b9966f02e552073dbcdcb16b23c
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-15 16:32:39 +0700

    Update function get_env(), need return string

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # This function to get .env variable configuration
 # @params var - fill with the same variable name in .env configuration
 def get_env(var: str):
-    return os.environ.get(var)
+    return str(os.environ.get(var))
 
 # This function to get date time now with custom format
 def date_now(pattern: str):
