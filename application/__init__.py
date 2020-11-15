8ace280ee36cfcd7b311d79d112cdfd0c3d3f293
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-15 11:39:46 +0700

    Adding configuration database with flask

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
+# Initialize and set configuration database with flask
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from .models.owner_model import OwnerModel
