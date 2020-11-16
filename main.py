2b42d28f375e3193fd5cf94ccf46665b598364bd
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 09:52:03 +0700

    Routing API

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from application.utils.security_util import access_key_owner
 from application.utils import get_post
 from application.utils import security_util
 
+# Route owner registration
 @app.route('/warungku/owner/auth/register', methods=['POST'])
 @access_key_owner
 async def owner_register():
