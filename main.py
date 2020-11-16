130a5c32e133a238a919b484e78e9f80119b19a1
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 10:36:23 +0700

    Fix and improvement little bit mistake owner_register

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from application import app
 from application.controllers import owner_controller
 from application.models.owner_model import OwnerModel
-from application.utils.security_util import access_key_owner
-from application.utils import get_post
-from application.utils import security_util
+from application.utils import get_post, security_util
 
 # Route owner registration
 @app.route('/warungku/owner/auth/register', methods=['POST'])
-@access_key_owner
-async def owner_register():
+@security_util.access_key_owner
+def owner_register():
     owner = OwnerModel(
         full_name=get_post('full_name'),
         email=get_post('email'),
         password=security_util.hash_password(get_post('password'))
     )
-    return await owner_controller.register(owner)
+    return owner_controller.register(owner)
 
 
 if __name__ == '__main__':
