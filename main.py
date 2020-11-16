33823eb1729afb027f3294a9445b73d49a443cb5
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 23:05:41 +0700

    Little bit improvement and adding store_name body

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from application import app
+from application import app, OwnerModel, StoreModel
 from application.controllers import owner_controller
-from application.models.owner_model import OwnerModel
-from application.utils import get_post, security_util
+from application.utils import get_post, security_util, get_unique_id
+
 
 # Route owner registration
 @app.route('/warungku/owner/auth/register', methods=['POST'])
 @security_util.access_key_owner
 def owner_register():
+    generated_id = get_unique_id()
+
     owner = OwnerModel(
+        id=generated_id,
         full_name=get_post('full_name'),
+        store=[StoreModel(
+            owner_id=generated_id, name=get_post('store_name')
+        )],
         email=get_post('email'),
         password=security_util.hash_password(get_post('password'))
     )
