5cc6a8c6a1ea2590a8643a7b5eb4c84c356f468e
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 23:32:09 +0700

    Adding owner login function

--- application/controllers/owner_controller.py
+++ application/controllers/owner_controller.py
@@ -1,12 +1,13 @@
 from http.client import HTTPException
 
 from application.models.owner_model import OwnerModel
-from application.utils import is_none, response_util
+from application.utils import is_none, response_util, security_util
 from application import db
+
 
 # This is function to do owner registration
 def register(owner: OwnerModel):
-    if is_none(owner.full_name) or is_none(owner.email) or is_none(owner.password):
+    if is_none(owner.full_name) or is_none(owner.store[0].name) or is_none(owner.email) or is_none(owner.password):
         return response_util.http_bad_request('Fill all the request body!')
     else:
         owner_query = OwnerModel.query.filter_by(email=owner.email).first()
@@ -21,3 +22,18 @@ def register(owner: OwnerModel):
                 return response_util.http_internal_server_error()
 
         return response_util.http_created('Owner account has been created!')
+
+
+# This function to do owner login
+def login(owner: OwnerModel):
+    if is_none(owner.email) or is_none(owner.password):
+        return response_util.http_bad_request('Fill all the request body!')
+    else:
+        owner_query = OwnerModel.query.filter_by(email=owner.email).first()
+        if not owner_query:
+            return response_util.http_not_acceptable('Account is not registered!')
+        else:
+            if not security_util.verify_password(owner_query.password, owner.password):
+                return response_util.http_unauthorized('Wrong password account!')
+            else:
+                return response_util.http_ok('Owner login success!')
