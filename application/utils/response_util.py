ac2776bb81de20b47d6b357c095c61ffbe43977c
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 08:48:32 +0700

    Adding handle response: bad request

--- application/utils/response_util.py
+++ application/utils/response_util.py
@@ -1,9 +1,19 @@
-from flask import jsonify, make_response
 from http import HTTPStatus
+
+from . import json_response
 
 # Handle response invalid api key
 def http_forbidden():
-    return make_response(jsonify({
-        'status' : 'Forbidden',
-        'message' : 'Invalid api key!'
-    })), HTTPStatus.FORBIDDEN
+    return json_response({
+        'status': 'Forbidden',
+        'message': 'Invalid api key!',
+        'data': None
+    }, HTTPStatus.FORBIDDEN)
+
+# Handle response bad data sent from client
+def http_bad_request(message: str, data = None):
+    return json_response({
+        'status': 'Bad Request',
+        'message': message,
+        'data': data
+    }, HTTPStatus.BAD_REQUEST)
