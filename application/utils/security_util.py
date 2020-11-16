1b473c9b662f2e2bd69fabb26dfec17c63c5b376
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 07:57:55 +0700

    Adding code comment to access_key_owner

--- application/utils/security_util.py
+++ application/utils/security_util.py
@@ -3,6 +3,7 @@ from flask import request
 
 from application.utils import get_env, response_util
 
+# Access key owner, using this to end point WarungKu-Owner
 def access_key_owner(view_function):
     @wraps(view_function)
     def decorated_function(*args, **kwargs):
