8e4a45c31515abffa85abbc4ebdefeac86146b01
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 08:11:35 +0700

    Adding function json_response(), to make output json

--- application/utils/__init__.py
+++ application/utils/__init__.py
@@ -3,6 +3,8 @@ import uuid
 import requests
 
 from datetime import datetime
+from flask import jsonify, make_response
+from http import HTTPStatus
 
 # This function to get .env variable configuration
 # @params var - fill with the same variable name in .env configuration
@@ -20,6 +22,10 @@ def get_ip_address():
 # This to generate unique id
 def get_unique_id():
     return str(uuid.uuid4())
+
+# This to make json response
+def json_response(response: dict, http_code: int):
+    return make_response(jsonify(response)), http_code
 
 # This to verify value is none or not
 def is_none(value):
