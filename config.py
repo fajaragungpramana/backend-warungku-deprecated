b5e388e2cfe088b5aff4627d047af55e30123591
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 09:42:21 +0700

    Remove unused resource

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from application.utils import get_env
 
 # This is to configuration database mysql with the api
 class Config(object):
-    APPLICATION_ROOT = get_env('APPLICATION_ROOT')
     SQLALCHEMY_DATABASE_URI = get_env('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = get_env('SQLALCHEMY_TRACK_MODIFICATIONS')
     SQLALCHEMY_RECORD_QUERIES = get_env('SQLALCHEMY_RECORD_QUERIES')
