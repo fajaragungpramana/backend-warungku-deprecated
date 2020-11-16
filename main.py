f0badcce57d3e73d3f589720a5abc070e200a54b
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-16 09:42:45 +0700

    Fix little bit mistake

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from application.utils.security_util import access_key_owner
 from application.utils import get_post
 from application.utils import security_util
 
+@app.route('/owner/auth/register', methods=['POST'])
 @access_key_owner
-@app.route('owner/auth/register', methods=['POST'])
 async def owner_register():
     owner = OwnerModel(
         full_name=get_post('full_name'),
         email=get_post('email'),
         password=security_util.hash_password(get_post('password'))
     )
-    return owner_controller.register(owner)
+    return await owner_controller.register(owner)
 
 
 if __name__ == '__main__':
