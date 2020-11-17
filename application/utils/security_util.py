9e7027f5c3661c3291bc3a8dfdb9ce855cd02f0a
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-17 09:59:55 +0700

    Adding access_token to generate jwt token

--- application/utils/security_util.py
+++ application/utils/security_util.py
@@ -1,8 +1,11 @@
 from functools import wraps
 from flask import request
+from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
 
 from . import get_env, response_util
+
 
 # Access key owner, using this to end point WarungKu-Owner
 def access_key_owner(view_function):
@@ -12,7 +15,9 @@ def access_key_owner(view_function):
             return view_function(*args, **kwargs)
         else:
             return response_util.http_forbidden()
+
     return decorated_function
+
 
 # Hashing password from user
 def hash_password(password: str):
@@ -21,6 +26,14 @@ def hash_password(password: str):
     else:
         return None
 
+
 # Verify password from user
 def verify_password(hashed_password: str, password: str):
     return check_password_hash(hashed_password, password)
+
+
+# Generate access token
+def access_token(account_id: str):
+    return create_access_token(
+        [account_id], fresh=True, expires_delta=timedelta(minutes=1)
+    )  # Token expired in 1 minutes
