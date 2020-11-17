4dcefcabc0ef8fcf6ce0418089f3974bbb74f54e
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-17 10:21:02 +0700

    Adding sent verification_code through email

--- application/controllers/owner_controller.py
+++ application/controllers/owner_controller.py
@@ -1,3 +1,5 @@
+import random
+
 from http.client import HTTPException
 
 from application.models.owner_model import OwnerModel
@@ -8,14 +10,6 @@ from application import db
 
 # This is function to do owner registration
 def register(owner: OwnerModel):
-    owner.verification.append(
-        VerificationModel(
-            owner_id=owner.id,
-            name=owner.full_name,
-            account='OWNER'
-        )
-    )
-
     if is_none(owner.full_name) or is_none(owner.store[0].name) or is_none(owner.email) or is_none(owner.password):
         return response_util.http_bad_request('Fill all the request body!')
     else:
@@ -23,6 +17,14 @@ def register(owner: OwnerModel):
         if owner_query:
             return response_util.http_not_acceptable('Owner with a same email is already exist!')
         else:
+            owner.verification = [(
+                VerificationModel(
+                    owner_id=owner.id,
+                    name=owner.full_name,
+                    account='OWNER'
+                )
+            )]
+
             try:
                 db.session.add(owner)
                 db.session.add(owner.store[0])
@@ -34,6 +36,7 @@ def register(owner: OwnerModel):
         return response_util.http_created('Owner account has been created!', {
             'id': owner.id
         })
+
 
 # This function to do owner login
 def login(owner: OwnerModel):
@@ -51,6 +54,17 @@ def login(owner: OwnerModel):
                     'id': owner_query.id
                 })
 
+
 # This function to sent a code verification through email
-def verification_code(id: str):
-    return 0
+def verification_code(account_id: str):
+    verification_query = VerificationModel.query.filter(owner_id=account_id).first()
+    if not verification_query:
+        return response_util.http_bad_request('account id not found!')
+    else:
+        generated_code = random.randint(000000, 999999)
+        verification_query.code = generated_code
+
+        try:
+            db.session.commit()
+        except HTTPException:
+            return response_util.http_internal_server_error()
