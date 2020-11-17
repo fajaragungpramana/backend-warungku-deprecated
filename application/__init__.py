7897cafab00844f128ac80dc62e13bdc4b37ebf0
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-17 11:11:26 +0700

    Initialized jwt and mail

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
@@ -8,6 +10,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
+mail = Mail(app)
 
 from .models.owner_model import OwnerModel
 from .models.store_model import StoreModel
