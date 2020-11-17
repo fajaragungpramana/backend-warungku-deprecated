5aa21f1b310e3f54c2d4c4bcab876d1634551e72
fajaragungpramana <fajar.agungpramana01@gmail.com>
2020-11-17 07:05:27 +0700

    Create a new table verifications

---
+++ application/models/verification_model.py
@@ -0,0 +1,12 @@
+from application import db
+
+class VerificationModel(db.Model):
+    __tablename__ = 'verifications'
+
+    row = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.String(100), db.ForeignKey('owners.id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    account = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.Boolean, default=False, nullable=False)
+    phone = db.Column(db.Boolean, default=False, nullable=False)
+    code = db.Column(db.Integer, nullable=False)
